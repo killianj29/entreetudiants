f334bdd9c9d020ff4990e753f537847ad0fd42a9
rONE1223 <lerwan1@gmail.com>
2019-06-21 18:02:08 +0200

    épuration page annonces.php

--- annonces.php
+++ annonces.php
@@ -110,194 +110,78 @@
 		</div>
 	</div>
 </section>
-<section class="section-sm">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="search-result bg-gray">
-					<h2>Results For "Electronics"</h2>
-					<p>123 Results on 12 December, 2017</p>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-3">
-				<div class="category-sidebar">
-					<div class="widget category-list">
-	<h4 class="widget-header">All Category</h4>
-	<ul class="category-list">
-		<li><a href="category.php">Laptops <span>93</span></a></li>
-		<li><a href="category.php">Iphone <span>233</span></a></li>
-		<li><a href="category.php">Microsoft  <span>183</span></a></li>
-		<li><a href="category.php">Monitors <span>343</span></a></li>
-	</ul>
-</div>
 
-<div class="widget category-list">
-	<h4 class="widget-header">Nearby</h4>
-	<ul class="category-list">
-		<li><a href="category.php">New York <span>93</span></a></li>
-		<li><a href="category.php">New Jersy <span>233</span></a></li>
-		<li><a href="category.php">Florida  <span>183</span></a></li>
-		<li><a href="category.php">California <span>120</span></a></li>
-		<li><a href="category.php">Texas <span>40</span></a></li>
-		<li><a href="category.php">Alaska <span>81</span></a></li>
-	</ul>
-</div>
+<section class="section-sm ml-5 mr-5">
+	<div class="product-grid-list">
+		<div class="row mt-30">
+			<?php
+				foreach ($annonces as $annonce) {
+			?>
+			<div class="col-sm-12 col-lg-4 col-md-6">
 
-<div class="widget filter">
-	<h4 class="widget-header">Show Produts</h4>
-	<select>
-		<option>Popularity</option>
-		<option value="1">Top rated</option>
-		<option value="2">Lowest Price</option>
-		<option value="4">Highest Price</option>
-	</select>
-</div>
-
-<div class="widget price-range">
-	<h4 class="widget-header">Price Range</h4>
-	<div class="block">
-		<b>$10</b>
-		<input id="ex2" type="text" class="span2" value="" data-slider-min="10" data-slider-max="1000" data-slider-step="5" data-slider-value="[250,450]"/> 
-		<b>$5000</b>
-	</div>
-</div>
-
-<div class="widget product-shorting">
-	<h4 class="widget-header">By Condition</h4>
-	<div class="form-check">
-	  <label class="form-check-label">
-	    <input class="form-check-input" type="checkbox" value="">
-	    Brand New
-	  </label>
-	</div>
-	<div class="form-check">
-	  <label class="form-check-label">
-	    <input class="form-check-input" type="checkbox" value="">
-	    Almost New
-	  </label>
-	</div>
-	<div class="form-check">
-	  <label class="form-check-label">
-	    <input class="form-check-input" type="checkbox" value="">
-	    Gently New
-	  </label>
-	</div>
-	<div class="form-check">
-	  <label class="form-check-label">
-	    <input class="form-check-input" type="checkbox" value="">
-	    Havely New
-	  </label>
-	</div>
-</div>
-
-				</div>
-			</div>
-			<div class="col-md-9">
-				<div class="category-search-filter">
-					<div class="row">
-						<div class="col-md-6">
-							<strong>Short</strong>
-							<select>
-								<option>Most Recent</option>
-								<option value="1">Most Popular</option>
-								<option value="2">Lowest Price</option>
-								<option value="4">Highest Price</option>
-							</select>
+				<!-- product card -->
+				<div class="product-item bg-light">
+					<div class="card">
+						<div class="thumb-content">
+							<!-- <div class="price">$200</div> -->
+							<a href="">
+								<img class="card-img-top img-fluid" src="<?php echo $annonce['image1']; ?>" alt="Card image cap">
+							</a>
 						</div>
-						<div class="col-md-6">
-							<div class="view">
-								<strong>Views</strong>
-								<ul class="list-inline view-switcher">
-									<li class="list-inline-item">
-										<a href="javascript:void(0);"><i class="fa fa-th-large"></i></a>
-									</li>
-									<li class="list-inline-item">
-										<a href="javascript:void(0);"><i class="fa fa-reorder"></i></a>
-									</li>
-								</ul>
-							</div>
+						<div class="card-body">
+						    <h4 class="card-title"><a href=""><?php echo $annonce['titre']; ?></a></h4>
+						    <ul class="list-inline product-meta">
+						    	<li class="list-inline-item">
+						    		<a href=""><i class="fa fa-folder-open-o"></i><?php echo $annonce['categorie']; ?></a>
+						    	</li>
+						    	<li class="list-inline-item">
+						    		<a href=""><i class="fa fa-calendar"></i><?php echo $annonce['date']; ?></a>
+						    	</li>
+						    </ul>
+						    <p class="card-text"><?php echo $annonce['description']; ?></p>
+						    <div class="product-ratings">
+						    	<ul class="list-inline"> 
+						    		<li class="list-inline-item <?php echo $annonce['note'] > 0 ? 'selected' : ''; ?>"><i class="fa fa-star"></i></li>
+						    		<li class="list-inline-item <?php echo $annonce['note'] > 1 ? 'selected' : ''; ?>"><i class="fa fa-star"></i></li>
+						    		<li class="list-inline-item <?php echo $annonce['note'] > 2 ? 'selected' : ''; ?>"><i class="fa fa-star"></i></li>
+						    		<li class="list-inline-item <?php echo $annonce['note'] > 3 ? 'selected' : ''; ?>"><i class="fa fa-star"></i></li>
+						    		<li class="list-inline-item <?php echo $annonce['note'] > 4 ? 'selected' : ''; ?>"><i class="fa fa-star"></i></li>
+						    	</ul>
+						    </div>
 						</div>
 					</div>
 				</div>
-				<div class="product-grid-list">
-					<div class="row mt-30">
-
+			</div>
 	<?php
-		foreach ($annonces as $annonce) {
+		}
 	?>
-	<div class="col-sm-12 col-lg-4 col-md-6">
-
-	<!-- product card -->
-	<div class="product-item bg-light">
-		<div class="card">
-			<div class="thumb-content">
-				<!-- <div class="price">$200</div> -->
-				<a href="">
-					<img class="card-img-top img-fluid" src="<?php echo $annonce['image1']; ?>" alt="Card image cap">
-				</a>
-			</div>
-			<div class="card-body">
-			    <h4 class="card-title"><a href=""><?php echo $annonce['titre']; ?></a></h4>
-			    <ul class="list-inline product-meta">
-			    	<li class="list-inline-item">
-			    		<a href=""><i class="fa fa-folder-open-o"></i><?php echo $annonce['categorie']; ?></a>
-			    	</li>
-			    	<li class="list-inline-item">
-			    		<a href=""><i class="fa fa-calendar"></i><?php echo $annonce['date']; ?></a>
-			    	</li>
-			    </ul>
-			    <p class="card-text"><?php echo $annonce['description']; ?></p>
-			    <div class="product-ratings">
-			    	<ul class="list-inline"> 
-			    		<li class="list-inline-item <?php echo $annonce['note'] > 0 ? 'selected' : ''; ?>"><i class="fa fa-star"></i></li>
-			    		<li class="list-inline-item <?php echo $annonce['note'] > 1 ? 'selected' : ''; ?>"><i class="fa fa-star"></i></li>
-			    		<li class="list-inline-item <?php echo $annonce['note'] > 2 ? 'selected' : ''; ?>"><i class="fa fa-star"></i></li>
-			    		<li class="list-inline-item <?php echo $annonce['note'] > 3 ? 'selected' : ''; ?>"><i class="fa fa-star"></i></li>
-			    		<li class="list-inline-item <?php echo $annonce['note'] > 4 ? 'selected' : ''; ?>"><i class="fa fa-star"></i></li>
-			    	</ul>
-			    </div>
-			</div>
 		</div>
 	</div>
-</div>
 
-<?php
-	}
-?>
-
-
-
-
-						</div>
-					</div>
-				</div>
-				<div class="pagination justify-content-center">
-					<nav aria-label="Page navigation example">
-						<ul class="pagination">
-							<li class="page-item">
-								<a class="page-link" href="#" aria-label="Previous">
-									<span aria-hidden="true">&laquo;</span>
-									<span class="sr-only">Previous</span>
-								</a>
-							</li>
-							<li class="page-item"><a class="page-link" href="#">1</a></li>
-							<li class="page-item active"><a class="page-link" href="#">2</a></li>
-							<li class="page-item"><a class="page-link" href="#">3</a></li>
-							<li class="page-item">
-								<a class="page-link" href="#" aria-label="Next">
-									<span aria-hidden="true">&raquo;</span>
-									<span class="sr-only">Next</span>
-								</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</div>
+	<div class="pagination justify-content-center">
+		<nav aria-label="Page navigation example">
+			<ul class="pagination">
+				<li class="page-item">
+					<a class="page-link" href="#" aria-label="Previous">
+						<span aria-hidden="true">&laquo;</span>
+						<span class="sr-only">Précédent</span>
+					</a>
+				</li>
+				<li class="page-item"><a class="page-link" href="#">1</a></li>
+				<li class="page-item active"><a class="page-link" href="#">2</a></li>
+				<li class="page-item"><a class="page-link" href="#">3</a></li>
+				<li class="page-item">
+					<a class="page-link" href="#" aria-label="Next">
+						<span aria-hidden="true">&raquo;</span>
+						<span class="sr-only">Suivant</span>
+					</a>
+				</li>
+			</ul>
+		</nav>
 	</div>
+
 </section>
+
 <!--============================
 =            Footer            =
 =============================-->
